1ffef8e362823eb4f3706f34f5fac8aa099ccd57
MaxAlex42 <m1sellner@edu.aau.at>
2024-04-10 14:20:41 +0200

    #21 - Stomp Client Grundaufbau
    Verbindung zu Server funktioniert, Requests werden auch abgeschickt, responses sind immer 'null'

--- app/src/main/java/at/aau/serg/websocketdemoapp/activities/MainActivity.java
+++ app/src/main/java/at/aau/serg/websocketdemoapp/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package at.aau.serg.websocketdemoapp.activities;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,40 +8,48 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import at.aau.serg.websocketdemoapp.R;
-import at.aau.serg.websocketdemoapp.networking.MessageHandler;
-import at.aau.serg.websocketdemoapp.networking.WebSocketClient;
-import at.aau.serg.websocketdemoapp.networking.WebSocketMessageHandler;
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.StompClient;
 
 public class MainActivity extends AppCompatActivity {
     EditText playerName;
     TextView errorText;
-    WebSocketClient webSocketClient;
-    WebSocketMessageHandler<String> handler;
+    StompClient stompClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        webSocketClient = new WebSocketClient("ws://10.0.2.2:8080/websocket-example-handler");
-        handler = new MessageHandler();
-        webSocketClient.connectToServer(handler);
+        stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://10.0.2.2:8080/websocket-example-handler");
+        stompClient.connect();
 
         errorText = findViewById(R.id.labelError);
         playerName = findViewById(R.id.playerName);
+
+        Button view = findViewById(R.id.buttonTutorial);
+        view.setOnClickListener(c -> {
+            new Thread(() -> {
+                final String[] result = {"test"};
+                try {
+                    stompClient.topic("/topic/hello-response").subscribe(stompMessage -> {
+                        Log.v("Resposne", stompMessage.getPayload());
+                        result[0] = stompMessage.getPayload().toString();
+                    });
+                    
+                    stompClient.send("/app/hello", "Hello, Server!");
+
+                    runOnUiThread(() -> {
+                        playerName.setText(result[0].toString());
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        });
 
         createButtonClicked();
         joinButtonClicked();
@@ -54,72 +61,26 @@ public class MainActivity extends AppCompatActivity {
             if (!playerName.getText().toString().isEmpty()) {
                 // navigation to create lobbyroom site
                 Intent intent = new Intent(MainActivity.this, Lobbyroom.class);
-
-                try {
-                    webSocketClient.sendMessageToServer("Hello, Server!");
-                } catch(Exception e) {
-                    Log.e("WebSocket", e.getMessage());
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                intent.putExtra("playerName", playerName.getText().toString());
-                intent.putExtra("lobbyCode", handler.getValue());
+                intent.putExtra("playerName", "");
+                intent.putExtra("lobbyCode", "");
                 startActivity(intent);
-            }else {
+            } else {
                 errorText.setVisibility(View.VISIBLE);
             }
         });
-
     }
 
     public void joinButtonClicked() {
         Button joinButton = findViewById(R.id.buttonJoin);
         joinButton.setOnClickListener(view -> {
             if (!playerName.getText().toString().isEmpty()) {
-                // navigation to create lobbyroom site
+                // navigation to join lobbyroom site
                 Intent intent = new Intent(MainActivity.this, JoinLobby.class);
                 intent.putExtra("playerName", playerName.getText().toString());
                 startActivity(intent);
-
-            }else {
+            } else {
                 errorText.setVisibility(View.VISIBLE);
             }
         });
     }
-
-
-    public void tutorialButtonClicked(View view) {
-        // Tutorial Button Click handling hier
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Disconnect from the WebSocket server when the activity is destroyed
-        try {
-            webSocketClient.disconnect();
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        }
-    }
-
-/*
-    private void connectToWebSocketServer() {
-        // register a handler for received messages when setting up the connection
-        networkHandler.connectToServer(this::messageReceivedFromServer);
-    }
-
-    private void sendMessage() {
-        networkHandler.sendMessageToServer("test message");
-    }
-
-    private void messageReceivedFromServer(String message) {
-        // TODO handle received messages
-        Log.d("Network", message);
-        textViewServerResponse.setText(message);
-    }
- */
 }
---
+++ app/src/main/java/at/aau/serg/websocketdemoapp/networking/StompHandler.java
@@ -0,0 +1,116 @@
+package at.aau.serg.websocketdemoapp.networking;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+
+import io.reactivex.Flowable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.StompClient;
+
+public class StompHandler {
+
+    // TODO use correct hostname:port
+    /**
+     * localhost from the Android emulator is reachable as 10.0.2.2
+     * https://developer.android.com/studio/run/emulator-networking
+     * "ws://10.0.2.2:8080/websocket-example-handler"
+     */
+    private StompClient stompClient;
+    private Gson gson = new Gson();
+
+    public StompHandler(String ip) {
+        stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, ip);
+    }
+
+
+    private void setLifecycle() {
+        stompClient.lifecycle().subscribe(lifecycleEvent -> {
+            switch (lifecycleEvent.getType()) {
+
+                case OPENED:
+                    Log.d("Network", "Stomp connection opened");
+                    break;
+
+                case ERROR:
+                    Log.e("Network", "Error", lifecycleEvent.getException());
+                    break;
+
+                case CLOSED:
+                    Log.d("Network", "Stomp connection closed");
+                    break;
+            }
+        });
+    }
+
+
+    public void connectToServer() {
+        if (stompClient != null) {
+            try {
+                stompClient.connect();
+                setLifecycle();
+                Log.d("Network", "Connected");
+            } catch (Exception e) {
+                Log.e("Network", e.getMessage());
+            }
+        }
+    }
+
+    public Flowable<String> createLobby(String userID, String userName) {
+        HashMap<String, String> payload = new HashMap<>();
+        payload.put("userID", userID);
+        payload.put("userName", userName);
+        String jsonPayload = gson.toJson(payload);
+
+        Disposable disposable = stompClient.send("/create_new_game", jsonPayload).subscribe();
+
+        return stompClient.topic("topic/lobby-created")
+                .map(topicMessage -> {
+                    Log.d("Response", topicMessage.toString());
+                    return topicMessage.getPayload();
+                })
+                .subscribeOn(Schedulers.io());
+    }
+
+    public Flowable<String> joinLobby(String lobbyCode, String userID, String userName) {
+        HashMap<String, String> payload = new HashMap<>();
+        payload.put("lobbyCode", lobbyCode);
+        payload.put("userID", userID);
+        payload.put("userName", userName);
+        String jsonPayload = gson.toJson(payload);
+
+        Disposable disposable = stompClient.send("/join_lobby", jsonPayload).subscribe();
+
+        return stompClient.topic("topic/lobby-joined")
+                .map(topicMessage -> {
+                    Log.d("Response", topicMessage.toString());
+                    return topicMessage.getPayload();
+                })
+                .subscribeOn(Schedulers.io());
+    }
+
+    public Flowable<String> helloMessage(String message) {
+        Log.d("Network", "helloMessage called");
+        return stompClient.topic("/topic/hello-response")
+                .map(topicMessage -> {
+                    Log.d("Network", "Received: " + topicMessage.getPayload());
+                    return topicMessage.getPayload();
+                })
+                .doOnSubscribe(disposable -> stompClient.send("/app/hello", message).subscribe())
+                .subscribeOn(Schedulers.io());
+    }
+
+    public void disconnect() {
+        if (stompClient != null) {
+            try {
+                stompClient.disconnect();
+            } catch (Exception e) {
+                Log.e("Network", e.getMessage());
+            }
+        }
+    }
+}
