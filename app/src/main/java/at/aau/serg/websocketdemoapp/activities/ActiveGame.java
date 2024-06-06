af9263626d87aae6423ad76996208454b5f29be8
franzhaimburger <frhaimburger@edu.aau.at>
2024-06-06 08:23:25 +0200

    Implement issue functionality (play gaia, choose color)

--- app/src/main/java/at/aau/serg/websocketdemoapp/activities/ActiveGame.java
+++ app/src/main/java/at/aau/serg/websocketdemoapp/activities/ActiveGame.java
@@ -3,16 +3,20 @@ package at.aau.serg.websocketdemoapp.activities;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -97,11 +101,69 @@ public class ActiveGame extends AppCompatActivity {
             int marginLeft = midPoint + (c - Math.round(gameData.getCardList().size() / 2f)) * overlapPx;
             float rotation = (c - Math.round(gameData.getCardList().size() / 2f)) * 0.75f;
             CardFragment cardFragment = CardFragment
-                    .newInstance(gameData.getCardList().get(c-1).toString(), cardWidthPx, marginLeft, rotation);
+                    .newInstance(gameData.getCardList().get(c-1).getImgPath(), cardWidthPx, marginLeft, rotation);
             cardFragment.setFlingListener(activeGameService);
             transaction.add(container.getId(), cardFragment, "card_" + (c));
         }
         transaction.commitAllowingStateLoss();
+    }
+
+    private void checkForGaiaAndChooseColor() {
+        if (activeGameService.isCurrentlyActivePlayer()
+                && gameData.findCardByCardName("card_gaia0") != null) {
+
+            activeGameService.setPreventCardFling(true);
+
+            runOnUiThread(() -> Toast.makeText(getApplicationContext(), getString(R.string.play_gaia), Toast.LENGTH_SHORT).show());
+
+            ImageView gaiaImage = findViewById(R.id.playedGaia);
+            gaiaImage.setImageResource(R.drawable.card_gaia0);
+            gaiaImage.setVisibility(View.VISIBLE);
+
+            LinearLayout playedGaiaColors = findViewById(R.id.playedGaiaColors);
+
+            int[] colorDrawables = {
+                    R.drawable.color_green,
+                    R.drawable.color_yellow,
+                    R.drawable.color_red,
+                    R.drawable.color_blue,
+                    R.drawable.color_purple
+            };
+
+            for (int color : colorDrawables) {
+                ImageView colorImage = new ImageView(this);
+                colorImage.setImageResource(color);
+                LinearLayout.LayoutParams circleParams = new LinearLayout.LayoutParams(
+                        (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, getResources().getDisplayMetrics()),
+                        (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, getResources().getDisplayMetrics())
+                );
+                circleParams.setMargins(8, 0, 8, 0); // Add margin between circles
+                colorImage.setLayoutParams(circleParams);
+
+                colorImage.setOnClickListener(v -> {
+                    onColorPicked(getResources().getResourceEntryName(color));
+                });
+
+                playedGaiaColors.addView(colorImage);
+                playedGaiaColors.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void onColorPicked(String pickedColor) {
+        Toast.makeText(this, "Picked :" + pickedColor, Toast.LENGTH_SHORT).show();
+
+        Card card = gameData.findCardByCardName("card_gaia0");
+        if (gameData.getCardList().remove(card)) {
+            Log.d("REMOVE CARD", "CARD REMOVED SUCCESSFULLY");
+        }
+
+        activeGameService.playCard("gaia", pickedColor, 0);
+        this.refreshActiveGame();
+
+        findViewById(R.id.playedGaia).setVisibility(View.INVISIBLE);
+        findViewById(R.id.playedGaiaColors).setVisibility(View.INVISIBLE);
+        activeGameService.setPreventCardFling(false);
     }
 
     @SuppressLint("DiscouragedApi")
@@ -112,6 +174,7 @@ public class ActiveGame extends AppCompatActivity {
             ImageView imageView = findViewById(imageViewIds[i]);
             int resId = getResources().getIdentifier(card.getImgPath(), "drawable", getPackageName());
             imageView.setImageResource(resId);
+            imageView.setZ(i);
             imageView.setVisibility(View.VISIBLE);
         }
     }
@@ -147,6 +210,7 @@ public class ActiveGame extends AppCompatActivity {
 
                         if (!isFinishing() && !isDestroyed()) {
                             runOnUiThread(this::refreshActiveGame);
+                            runOnUiThread(this::checkForGaiaAndChooseColor);
                         }
                     }
                 }))).start();
